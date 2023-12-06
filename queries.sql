956d5807ffd53c8a5101643caf0fe8d31026a97c
monicasrinivasan27 <srinivasan.monica@gmail.com>
2023-12-05 18:37:05 -0600

    Done with the Changes.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 --Part 1
+--jobs
+--id int
+--employer varchar (255)
+--name varchar (255)
+--skills varchar(255)
 
 --Part 2
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-
+Drop table job;
 --Part 4
+SELECT * FROM skill LEFT JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
